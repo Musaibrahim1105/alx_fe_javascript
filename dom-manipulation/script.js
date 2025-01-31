4d1535a78d2222624d8a329c5aa07db4cdc6eda4
Musa Ibrahim <musaibrahim1105@gmail.com>
2025-01-31 08:54:27 +0100

    all file added

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,1 +1,6 @@
 
+<div>
+    <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+    <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+    <button onclick="addQuote()">Add Quote</button>
+  </div>
